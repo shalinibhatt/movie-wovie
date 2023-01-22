7ef5a5ab26cc940a308ce9429bec1ea341003653
Shalini Bhatt <shalinibhatt6263@gmail.com>
2023-01-22 18:58:21 +0530

    *

--- src/components/Actor/index.js
+++ src/components/Actor/index.js
@@ -5,7 +5,7 @@ import { Wrapper, Image } from "./Actor.styles";
 
 const Actor = ({ name, character, imageUrl }) => (
   <Wrapper>
-    <Image src={imageUrl} alt="actor-thumb" />
+    <Image  src={imageUrl} alt="actor-thumb" />
     <h3>{name}</h3>
     <p>{character}</p>
   </Wrapper>
